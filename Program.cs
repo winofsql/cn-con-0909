a29969fb981b6a1b732693ca78966732a1744df0
sworc <lightbox@nifty.com>
2021-09-09 16:39:10 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace cs_con_0909
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("こんにちは");
+
+            string a = "就活キックオフ";
+
+            Console.WriteLine( a );
+
+            string[] b = { "AB", "CD", "EX" };
+
+            for( int i = 0; i < b.Length; i++ )
+            {
+                Console.Write( b[i] );
+            }
+
+            Console.WriteLine();
+
+            foreach ( string value in b )
+            {
+                Console.Write( value );
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
